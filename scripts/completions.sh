4d62967fe753abf68d1126fe83d162454afbb913
João Cerqueira <joao@cerqueira.io>
2022-05-21 09:43:13 +0100

    build: add completion scripts

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -3,5 +3,5 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-	go run cmd/rsr/rsr.go completion "$sh" >"completions/rsr.$sh"
+	go run cmd/rsr/rsr.go completion "$sh" > "completions/rsr.$sh"
 done
